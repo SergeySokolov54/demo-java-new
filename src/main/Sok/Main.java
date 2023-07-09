6c16b1073dfa7a614b7fd808a26ba5c5b32be1f5
SergeySokolov54 <efgr97@mail.ru>
2023-07-09 23:19:00 +0300

    added my package

--- src/main/Sok/Main.java
+++ src/main/Sok/Main.java
@@ -3,9 +3,10 @@ package src.main.Sok;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
+
 public class Main {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("pokr");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("src.main.Sok");
         Car car = context.getBean(Car.class);
         car.zv();// для масштаба
     }
